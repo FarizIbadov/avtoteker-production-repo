a1db842cd0f428acfd03c62bbb0121491f4a1baa
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-01-08 16:54:21 +0400

    beta:10

--- mysite/main_site/templatetags/size_parser.py
+++ mysite/main_site/templatetags/size_parser.py
@@ -7,21 +7,33 @@ register = template.Library()
 
 
 @register.simple_tag(name="parse_size")
-def parse_size():
-    width_obj = SizeField("width","en")
-    height_obj = SizeField("height","hündürlük")
-    radius_obj = SizeField("radius","radius")
-    return (width_obj, height_obj, radius_obj)
+def parse_size(request):
+    width_text = "en"
+    width_def = ""
+    if request.GET.get('width'):
+        width_def = request.GET.get('width')
 
-@register.simple_tag(name="get_brand")
-def get_brand():
-    return Brand.objects.all()
+    height_text = "hündürlük"
+    height_def = ""
+    if request.GET.get('height'):
+        height_def = request.GET.get('height')
+
+    radius_text = "radius"
+    radius_def = ""
+    if request.GET.get('radius'):
+        radius_def = request.GET.get('radius')
+
+    width_obj = SizeField("width",width_text,width_def)
+    height_obj = SizeField("height",height_text,height_def)
+    radius_obj = SizeField("radius",radius_text,radius_def)
+    return (width_obj, height_obj, radius_obj)
 
 
 class SizeField:
     queryset = Size.objects.all()
 
-    def __init__(self, field,name):
+    def __init__(self, field,name,default=""):
+        self.default = default
         self.field = field
         self.name = name
         self.list = self.get_field_list()
