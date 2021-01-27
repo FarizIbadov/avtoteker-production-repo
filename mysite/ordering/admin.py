ede111da40e49078e4410353cd014f177604387d
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-01-27 13:34:38 +0400

    order

--- mysite/ordering/admin.py
+++ mysite/ordering/admin.py
@@ -4,3 +4,4 @@ from .models import Order
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
     readonly_fields = ['tire','email','phone','payment_type']
+    list_filter = ['remember_me']
--- mysite/ordering/models.py
+++ mysite/ordering/models.py
@@ -10,6 +10,7 @@ class Order(models.Model):
         (4,'BirKart / TamKart ilə'),
     ]
 
+    name = models.CharField(max_length=20,null=True,blank=True)
     tire = models.ForeignKey(Tire,on_delete=models.CASCADE)
     quantity = models.PositiveSmallIntegerField(default=1)
     phone = models.CharField(max_length=20)
@@ -17,6 +18,8 @@ class Order(models.Model):
     payment_type = models.PositiveSmallIntegerField(choices=PAYMENT_CHOICES,default=1) 
     is_purchased = models.BooleanField(default=False)
     order_date = models.DateTimeField(default=timezone.now)
+    note = models.TextField(blank=True,null=True)
+    remember_me = models.BooleanField(default=False)
 
     def __str__(self):
         return '%s %s %s' % (self.tire.brand,self.tire.serie,self.tire.size)
