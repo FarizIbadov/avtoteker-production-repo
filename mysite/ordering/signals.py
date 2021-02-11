d51fbfcbf445c3aa4f6c843048284664ec79c928
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-02-11 11:49:26 +0400

    order-changes

--- mysite/ordering/signals.py
+++ mysite/ordering/signals.py
@@ -17,34 +17,7 @@ from django.contrib.staticfiles import finders
 import os
 
 @receiver(post_save, sender=Order)
-def order_save(sender, instance, created, **kwargs):
-    if created and instance.email:
-        img_url = finders.find(path='img/logo.png',all=False)
-        img_data = open(img_url,'rb').read()
-        domain = os.environ.get('HOST',"http://localhost:8000")
-        context = {'content':"Test",'domain':domain}
-        html_content = MIMEMultipart(_subtype='related')
-        body = MIMEText(get_template('email.html').render(context),_subtype="html")
-        html_content.attach(body)
-
-        img = MIMEImage(img_data,'png')
-        img.add_header('Content-Id', '<logoimage>')
-        img.add_header("Content-Disposition", "inline", filename="logo.png")
-        html_content.attach(img)
-        
-        email = EmailMultiAlternatives(
-            "Payment Bill",
-            None,
-            settings.EMAIL_HOST_USER,
-            [instance.email],
-        )
-        email.attach(html_content)
-        email.send()
-
-    elif not created and instance.is_purchased:
-        order_qtn = instance.quantity
-        tire = instance.tire
-        qtn = tire.quantity - order_qtn
-        instance.tire.quantity = qtn
-        instance.tire.save()
+def order_save(sender, instance:Order, created, **kwargs):
+    if created:
+        instance.note = instance.tire
     
