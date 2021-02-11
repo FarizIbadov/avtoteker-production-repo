87c2aaad921ceda1ae6dd2ad91b9a9db4c0afbf0
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-02-11 11:54:36 +0400

    main-site

--- mysite/main_site/urls.py
+++ mysite/main_site/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
-from .views import MainPage, MainListView, MainDetailView
+from .views import MainPage, MainListView, MainDetailView,MainSearchView
 from .sitemaps import TireSitemap
 
 sitemaps = {
@@ -12,5 +12,6 @@ urlpatterns = [
     path("sitemap.xml",sitemap,{"sitemaps":sitemaps}),
     path("tires/", MainListView.as_view(), name="list"),
     path("tires/<int:pk>/", MainDetailView.as_view(), name="detail"),
+    path("search-tire/",MainSearchView.as_view(),name="search-tire"),
     path('order/',include('ordering.urls'))
 ]
