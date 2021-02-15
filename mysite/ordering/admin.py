235b319720d80db0f124c0091be873238a7fa676
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-02-15 13:08:44 +0400

    order and tire

--- mysite/ordering/admin.py
+++ mysite/ordering/admin.py
@@ -5,11 +5,23 @@ from django.utils.html import format_html
 
 @admin.register(Order)
 class OrderAdmin(admin.ModelAdmin):
-    readonly_fields = ['tire','email','phone_number','payment_type','name']
+    readonly_fields = ['product','email','phone_number','payment_type','name']
     list_filter = ['remember_me']
-    exclude = ['phone']
+    list_display = ['product_title']
+    exclude = ['phone','tire','product_title','product_link']
 
 
-    def phone_number(self,obj):
+    def phone_number(self,obj:Order):
         link = "<a href='tel:%s'>%s</a>" % (obj.phone,obj.phone)
         return format_html(link) 
+    
+    def product(self,obj:Order):
+        title = ""
+        tire = obj.tire
+        if tire:
+            title = tire
+        if not title:
+            title = obj.product_title
+
+        link = "<a href='%s'>%s</a>" % (obj.product_link,title)
+        return format_html(link)
