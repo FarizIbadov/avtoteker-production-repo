64f7d357b1aee3d029a9c7bc9a1bfca0cf71a29b
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-02-27 18:36:11 +0400

    h1:2

--- mysite/oilapp/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import OilMainView,OilListView
-
-urlpatterns = [
-    path("oil/",OilMainView.as_view(),name="oil"),
-    path("oil-list/",OilListView.as_view(),name="oil-list")
-]
-
