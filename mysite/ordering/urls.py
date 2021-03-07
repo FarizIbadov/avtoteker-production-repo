62b5b4e1b8508babcb2d0b82c929e455555940fd
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-03-07 21:56:14 +0400

    oilapp-order

--- mysite/ordering/urls.py
+++ mysite/ordering/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import OrderCreateView,OilOrderCreateView
 
 urlpatterns = [
-    path("",OrderCreateView.as_view(),name="order"),
+    path("",OrderCreateView.as_view(),name="tire-order"),
     path("oil/",OilOrderCreateView.as_view(),name="oil-order"),
 ]
