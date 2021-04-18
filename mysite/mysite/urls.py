395b5c28d40b9c8f2a2e56120827a341faa2c914
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-04-18 10:49:09 +0400

    main url admin

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("", include("main_site.urls")),
     path("",include("oilapp.urls")),
     path("admin/", include("custom_admin.urls")),
-    path("base-admin/", admin.site.urls),
+    path("avtoadmin/", admin.site.urls),
     path("",include('wheel_size.urls')),
     path("",include("campaign.urls")),
     path("",include("news.urls")),
