46817b4791e5663af6d70902471e9ac6a3e9b1b0
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-06-03 15:26:47 +0400

    taksit kredit interval add

--- mysite/kredit_taksit/admin.py
+++ mysite/kredit_taksit/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
-from .models import KreditTaksitImage
+from .models import KreditTaksitImage,KreditTaksitInterval
 
 @admin.register(KreditTaksitImage)
 class KreditTaksitImageAdmin(admin.ModelAdmin):
     list_display = ("name","image")
+
+@admin.register(KreditTaksitInterval)
+class KreditTaksitIntervalAdmin(admin.ModelAdmin):
+    list_display = ("interval",)
