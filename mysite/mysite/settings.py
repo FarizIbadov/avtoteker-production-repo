cbc5570c2d2ffe2bdf5409e7b083ca38497c66cf
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-06-09 17:34:15 +0400

    order and comming soon added 2

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -27,7 +27,7 @@ elif ALLOWED_HOSTS_ENV:
 if DEBUG:
     CORS_ALLOWED_ORIGINS = ['http://localhost:3000','http://localhost:8000']
 else:
-    CORS_ALLOWED_ORIGINS = ALLOWED_HOSTS.copy()
+    CSRF_TRUSTED_ORIGINS = ALLOWED_HOSTS
 
 # Application definition
 
