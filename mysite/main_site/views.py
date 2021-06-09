bf8513750df7025733507b3572d99e1834d73921
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-06-09 17:24:52 +0400

    order and comming soon added

--- mysite/main_site/views.py
+++ mysite/main_site/views.py
@@ -28,7 +28,6 @@ class MainListView(FilterBySizeMixin, ListView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = OrderForm
         context['tire_search_title'] = "<h2 class='tire-search__heading'>Seçim edin</h2>"
         return context
 
@@ -44,60 +43,6 @@ class MainDetailView(DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['form'] = OrderForm
         context['tire_search_title'] = "<h2 class='tire-search__heading'>Seçim edin</h2>"
         return context
-
-
-# class MainSearchView(View):
-#     def get(self,request,*args,**kwargs):
-#         list_query_list = [] 
-#         search = request.GET['search'].strip()
-#         regex = re.compile(r"\s+")
-#         search_list = regex.split(search) 
-
-#         for search_query in search_list:
-#             size_query = self.check_for_size(search_query)
-#             if size_query:
-#                 list_query_list.append(size_query)
-#                 continue
-
-#             brand_query = self.check_for_brand(search_query)
-#             if brand_query:
-#                 list_query_list.append(brand_query)
-#                 continue
-            
-#             serie_query = self.check_for_serie(search_query)
-#             if serie_query:
-#                 list_query_list.append(serie_query)
-#                 continue
-
-#         list_query = "&".join(list_query_list)
-#         list_path = reverse('list')
-#         list_url = "?".join([list_path,list_query])
-#         return redirect(list_url)
-
-#     def check_for_size(self,query):
-#         regex = re.compile(r"(\d{3})?(\d{0,2})?(\d{0,3})?")
-#         groups = regex.search(query)
-
-#         if not groups:
-#             return None
-
-#         width,height,radius = groups.groups()
-
-#         if not width and not height and not radius:
-#             return None
-
-#         width_query = "width=%s" % width
-#         height_query = "height=%s" % height
-#         radius_query = "radius=%s" % radius
-#         return "&".join([width_query,height_query,radius_query])
-
-#     def check_for_brand(self,query):
-#         brand_title = Brand.objects.annotate(similarity=TrigramSimilarity('title',query)).all().order_by('-similarity','-order_number').first().title
-#         return "brand=%s" % brand_title.lower()
-
-#     def check_for_serie(self,query):
-#         serie_title = Serie.objects.annotate(similarity=TrigramSimilarity('title',query)).all().order_by('-similarity').first().title
-#         return "serie=%s" % serie_title.lower()
+        
--- mysite/ordering/views.py
+++ mysite/ordering/views.py
@@ -1,22 +1,14 @@
-from django.shortcuts import redirect
-from django.urls import reverse
-from django.views.generic import CreateView
-from .forms import OrderForm,OilOrderForm
+from rest_framework import permissions
+from rest_framework.generics import CreateAPIView
+from . import serializers
 
-class OrderCreateView(CreateView):
-    form_class = OrderForm
 
-    def get_success_url(self):
-        return reverse('list')
+class TireOrderAPIView(CreateAPIView):
+    serializer_class = serializers.TireOrderSerializer
+    permission_classes = [permissions.AllowAny]
+    
 
-    def form_invalid(self,form):
-        return redirect('home')
-
-class OilOrderCreateView(CreateView):
-    form_class = OilOrderForm
-
-    def get_success_url(self):
-        return reverse('oil-list')
-
-    def form_invalid(self,form):
-        return redirect('oil')
+class OilOrderAPIView(CreateAPIView):
+    serializer_class = serializers.OilOrderSerializer
+    permission_classes = [permissions.AllowAny]
+    
