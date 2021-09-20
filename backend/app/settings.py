3ce716c99952beb99636b6c258c1e365ccad1d4d
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-09-20 16:47:32 +0400

    internalization add 3

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -206,21 +206,21 @@ LOCALE_PATHS = (
 
 MODELTRANSLATION_DEFAULT_LANGUAGE = "az"
 
-# LOGGING = None
+LOGGING = None
 
-# if not DEBUG:
-#     LOGGING = {
-#         'version': 1,
-#         'disable_existing_loggers': False,
-#         'handlers': {
-#             'console': {
-#                 'class': 'logging.StreamHandler',
-#             },
-#         },
-#         'loggers': {
-#             'django': {
-#                 'handlers': ['console'],
-#                 'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-#             },
-#         },
-#     }
+if not DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+                'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+            },
+        },
+    }
