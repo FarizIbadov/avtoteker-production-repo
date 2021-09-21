1e20a27a26d4f6af7701af0787e0aec54ec6f916
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-09-21 11:36:29 +0400

    locale change 2

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,8 +2,8 @@
 
 set -e
 
+python manage.py makemessages --all
 python manage.py collectstatic --noinput
 python manage.py migrate --run-syncdb
-python manage.py makemessages --all
 
 uwsgi --socket :8000 --master --enable-threads --module app.wsgi
