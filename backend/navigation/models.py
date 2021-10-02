1c014083a2df1f0518e54ffb8c05342ec4a4079a
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-10-02 16:30:46 +0400

    db change

--- backend/navigation/models.py
+++ backend/navigation/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class NavigationLink(models.Model):
     title = models.CharField(max_length=20, null=True)
-    link = models.CharField(max_length=200)
+    link = models.CharField(max_length=200,null=True)
     order_number = models.PositiveSmallIntegerField(default=0)
     active = models.BooleanField(default=True)
 
