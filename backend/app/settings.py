dd9dc516d31b0f450f3d6a6b79d7cd098a53d9b7
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2021-10-17 12:03:38 +0400

    logging remove

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -211,22 +211,22 @@ ROSETTA_UWSGI_AUTO_RELOAD = ROSETTA_WSGI_AUTO_RELOAD
 
 # DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGGING = None
+# LOGGING = None
 
-if not DEBUG:
-    LOGGING = {
-        'version': 1,
-        'disable_existing_loggers': False,
-        'handlers': {
-            'console': {
-                'class': 'logging.StreamHandler',
-            },
-        },
-        'loggers': {
-            'django': {
-                'handlers': ['console'],
-                'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-            },
-        },
-    }
+# if not DEBUG:
+#     LOGGING = {
+#         'version': 1,
+#         'disable_existing_loggers': False,
+#         'handlers': {
+#             'console': {
+#                 'class': 'logging.StreamHandler',
+#             },
+#         },
+#         'loggers': {
+#             'django': {
+#                 'handlers': ['console'],
+#                 'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+#             },
+#         },
+#     }
 
