d73d469194f5d916cff8ae03b40777757d1e1883
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2022-02-16 23:39:19 +0400

    nav fix

--- backend/navigation/templatetags/navigation_list.py
+++ backend/navigation/templatetags/navigation_list.py
@@ -5,7 +5,6 @@ register = template.Library()
 
 @register.simple_tag(name="get_navigation_list")
 def get_navigation_list():
-
     return models.NavigationLink.objects.filter(active=True).exclude(title__isnull=True)
 
 @register.simple_tag(name="get_logo")
