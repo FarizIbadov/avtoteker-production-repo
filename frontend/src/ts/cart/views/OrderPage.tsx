decd8f261406fa77a05f66731b08e5335619dce2
FarizIbadov <51949551+FarizIbadov@users.noreply.github.com>
2022-07-13 23:44:35 +0400

    commit

--- frontend/src/ts/cart/views/OrderPage.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const OrderPage: React.FC = () => {
-  return <div>Order Page</div>;
-};
-
-export default OrderPage;
--- scripts/entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -e
-
-python manage.py makemessages --all
-python manage.py collectstatic --noinput
-python manage.py migrate --run-syncdb
-
-uwsgi --socket :8000 --master --enable-threads --module app.wsgi
