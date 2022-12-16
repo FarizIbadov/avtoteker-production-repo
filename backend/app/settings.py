fb527f44dc2d30ea5a6928533a06f157585a048f
Fariz Ibadov <farizibadov@Farizs-16-inch-MacBook-Pro.local>
2022-12-16 18:11:26 +0300

    logging

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -223,21 +223,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 SESSION_COOKIE_AGE = 1209600 * 4
 
-# LOGGING = None
+LOGGING = None
 
-# if not DEBUG:
-#     LOGGING = {
-#         'version': 1,
-#         'disable_existing_loggers': False,
-#         'handlers': {
-#             'console': {
-#                 'class': 'logging.StreamHandler',
-#             },
-#         },
-#         'loggers': {
-#             'django': {
-#                 'handlers': ['console'],
-#                 'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
-#             },
-#         },
-#     }
+if not DEBUG:
+    LOGGING = {
+        'version': 1,
+        'disable_existing_loggers': False,
+        'handlers': {
+            'console': {
+                'class': 'logging.StreamHandler',
+            },
+        },
+        'loggers': {
+            'django': {
+                'handlers': ['console'],
+                'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+            },
+        },
+    }
