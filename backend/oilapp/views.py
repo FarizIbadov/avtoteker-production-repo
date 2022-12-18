d463255b9e326572212adebd553d6a6b739c7c0c
Fariz Ibadov <farizibadov@Farizs-16-inch-MacBook-Pro.local>
2022-12-18 14:36:55 +0300

    filename change

--- backend/oilapp/views.py
+++ backend/oilapp/views.py
@@ -1,22 +1,25 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView,ListView,DetailView
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Oil
 
+
 class OilMainView(TemplateView):
-    template_name = "oil/oil-main.html"
+    template_name = "oil/main.html"
+
 
 class OilListView(ListView):
-    template_name = "oil/oil-list.html"
+    template_name = "oil/list.html"
 
-    def get_queryset(self,*args,**kwargs):
+    def get_queryset(self, *args, **kwargs):
         viscosity = self.request.GET.get("viscosity")
         kwargs = {}
         if viscosity:
             kwargs['viscosity'] = viscosity
         return Oil.objects.available(**kwargs)
-        
+
+
 class OilDetailView(DetailView):
-    template_name = "oil/oil-detail.html"
+    template_name = "oil/detail.html"
 
     def get_object(self):
         pk = self.kwargs['pk']
