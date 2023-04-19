0c25090769a46a63f9fc1e871b7fff810c60e7be
Fariz Ibadov <farizibadov@Farizs-16-inch-MacBook-Pro.local>
2023-04-20 00:58:40 +0300

    about log

--- backend/about/views.py
+++ backend/about/views.py
@@ -1,4 +1,5 @@
 from django.views.generic import TemplateView
+from django.utils.translation import gettext as _ 
 from . import models
 
 class AboutView(TemplateView):
@@ -7,4 +8,6 @@ class AboutView(TemplateView):
     def get_context_data(self):
         context = super().get_context_data()
         context['about'] = models.About.objects.filter(active=True).first()
+        title = _("Seçim edin")
+        context['tire_search_title'] = f"""<h2 class='tire-search__heading'>{title}</h2>"""
         return context
